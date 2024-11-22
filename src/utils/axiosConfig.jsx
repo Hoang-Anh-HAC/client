04ea7fc85865d8cb346bc7521355d227d85900cf
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2024-11-22 16:52:36 +0700

    Update Link Demo

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// export default axios.create({
+//   baseURL: "http://localhost:5000/api/",
+// });
+
 export default axios.create({
-  baseURL: "http://localhost:5000/api/",
+  baseURL: "https://server-orlc.onrender.com/api/",
 });

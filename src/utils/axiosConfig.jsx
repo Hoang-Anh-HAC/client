45b216777df16a2fdaa416d67610c32901bf3279
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2024-12-07 09:37:49 +0700

    Update axios configuration to use production API URL

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:5000/api/",
-});
-
 // export default axios.create({
-//   baseURL: "https://server-orlc.onrender.com/api/",
+//   baseURL: "http://localhost:5000/api/",
 // });
+
+export default axios.create({
+  baseURL: "https://server-orlc.onrender.com/api/",
+});

172c3f3ebe1cb7adb5f7b9aaf9a26aaa8fcad825
HvThang02 <88171110+HvThang02@users.noreply.github.com>
2025-04-17 09:37:29 +0700

    fix: update axios base URL to use the correct production server address

--- src/utils/axiosConfig.jsx
+++ src/utils/axiosConfig.jsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// export default axios.create({
-//   baseURL: "https://api.hac.com.vn/api/",
-// });
-
 export default axios.create({
-  baseURL: "http://112.78.1.194:5000/api/",
+  baseURL: "https://api.hac.com.vn/api/",
 });
+
+// export default axios.create({
+//   baseURL: "http://112.78.1.194:5000/api/",
+// });
